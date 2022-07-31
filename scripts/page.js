5a93878cc3665f4216e5ef4f7bd532181fe7c2bf
Dunky-Z <254758318@qq.com>
2022-07-31 21:29:09 +0800

    add video background

--- scripts/page.js
+++ scripts/page.js
@@ -1,5 +1,4 @@
 hexo.extend.filter.register('theme_inject', function(injects) {
-  //injects.header.file('default', 'source/_inject/test1.ejs', { key: 'value' }, -1);
-  injects.bodyBegin.file('default', "C:\\Develop\\Dunky-Z.github.io\\node_modules\\hexo-theme-fluid\\source\\_inject\\bodyBegin.ejs");
-  injects.header.file('video-banner', 'C:\\Develop\\Dunky-Z.github.io\\node_modules\\hexo-theme-fluid\\source\\_inject\\header.ejs', { key: 'value' }, -1);
+  injects.bodyBegin.file('default', "./source/_inject/bodyBegin.ejs");
+  injects.header.file('banner_video_insert_here', "./source/_inject/header.ejs", { key: 'value' }, -1);
 });
